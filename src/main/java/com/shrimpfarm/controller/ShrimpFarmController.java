1d386819d78373e66760e89269f64ff5a0bb2e6f
Rajesh Kumar <rajekuma@lowes.com>
2020-09-04 07:57:19 +0530

    Commit to add the get farm size call

--- src/main/java/com/shrimpfarm/controller/ShrimpFarmController.java
+++ src/main/java/com/shrimpfarm/controller/ShrimpFarmController.java
@@ -1,21 +1,21 @@
 package com.shrimpfarm.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.shrimpfarm.exception.ShrimpFarmException;
 import com.shrimpfarm.model.Farms;
+import com.shrimpfarm.model.Ponds;
 import com.shrimpfarm.repository.ShrimpFarmRepository;
+import com.shrimpfarm.repository.ShrimpPondsRepository;
 
 @Controller
 @RequestMapping("/farms/")
@@ -23,6 +23,11 @@ public class ShrimpFarmController {
 
 	@Autowired
 	private ShrimpFarmRepository farmRepository;
+
+	@Autowired
+	private ShrimpPondsRepository pondsRepository;
+
+	int size = 0;
 
 	@GetMapping("showForm")
 	public String showZipcodeForm(Farms farms) {
@@ -43,18 +48,18 @@ public class ShrimpFarmController {
 		this.farmRepository.save(farms);
 		return "redirect:listAllFarms";
 	}
-	
+
 	@GetMapping("edit/{id}")
-	public String showUpdateForm(@PathVariable ("id") String id, Model model) throws ShrimpFarmException {
+	public String showUpdateForm(@PathVariable("id") String id, Model model) throws ShrimpFarmException {
 		Farms farm1 = farmRepository.findById(id).orElseThrow(() -> new ShrimpFarmException("Farm not found :: " + id));
-		
+
 		model.addAttribute("farms", farm1);
 		return "update-farm";
 	}
 
 	@PostMapping("updateFarm/{id}")
 	public String updateFarm(@PathVariable(value = "id") String id, Farms farms, BindingResult result, Model model) {
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			farms.setId(id);
 			return "update-farm";
 		}
@@ -70,6 +75,18 @@ public class ShrimpFarmController {
 		farmRepository.delete(farms);
 
 		model.addAttribute("farms", this.farmRepository.findAll());
+		return "index";
+	}
+
+	@GetMapping("getFarmSize/{id}")
+	public String getFarmSize(@PathVariable("id") String id, Model model) throws ShrimpFarmException {
+		Farms farms = farmRepository.findById(id).orElseThrow(() -> new ShrimpFarmException("Farm not found :: " + id));
+		List<Ponds> farm1 = pondsRepository.findByFarmId(id);
+		farm1.forEach(f -> {
+			size += Integer.parseInt(f.getPondSize());
+		});
+		farms.setFarmSize(String.valueOf(size));
+		model.addAttribute("farms", farms);
 		return "index";
 	}
 }
